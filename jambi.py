f8a5e5bd629cefe75d9f443077c4985771e5431f
Zach Kruchoski <kruchone@gmail.com>
2016-10-21 21:54:45 -0400

    first pass at upgrades

--- jambi.py
+++ jambi.py
@@ -1,10 +1,15 @@
 #! /usr/bin/env python3
 import argparse
 import configparser
+import importlib
 import logging
+import os
+import re
 import sys
+import time
 
-from peewee import CharField, Model, PostgresqlDatabase, ProgrammingError
+from peewee import CharField, IntegrityError, Model, PostgresqlDatabase, ProgrammingError
+from playhouse.migrate import PostgresqlMigrator, migrate
 
 
 _db = PostgresqlDatabase(None)
@@ -24,16 +29,71 @@ class Jambi(object):
     """A database migration helper for peewee."""
     def __init__(self):
         logging.basicConfig(level=logging.DEBUG)
+        logging.getLogger('peewee').setLevel(logging.INFO)
         self.logger = logging.getLogger('jambi')
         self.db, self.db_schema = self.__get_db_and_schema_from_config()
 
     def upgrade(self, ref):
-        """migrate the database to the supplied reference hash"""
-        ref = ref or 'head'
-        self.logger.info('migrating to "{}"'.format(ref))
-        self.db.connect()
-        self.db.close()
+        """migrate the database to the supplied version"""
+        current_ref = self.inspect()
+        if current_ref is None:
+            self.logger.error('upgrade halted: you must initialize jambi first')
+            return
+        migrations = self.find_migrations()
+        migrations = tuple(filter(lambda x: x[1] > current_ref, migrations))
+        if any(migrations):
+            self.logger.info('migrating to "{}"'.format(ref))
+            self.db.connect()
+            with self.db.atomic():
+                for n, v, m in migrations:
+                    self.logger.info('upgrading to version {}'.format(v))
+                    migrator = PostgresqlMigrator(self.db)
+                    upgrades = m.upgrade(migrator)
+                    migrate(*upgrades)
+                print(migrations[-1][1])
+                self.__set_version(migrations[-1][1])
+            self.db.close()
+            self.logger.info(self.inspect())
+        else:
+            self.logger.info('you are already up to date')
         return
+
+    def downgrade(self, ref):
+        """downgrade the db to the supplied version"""
+        return NotImplemented
+
+    def latest(self):
+        """returns the latest version in the migrations folder"""
+        return NotImplemented
+
+    def find_migrations(self):
+        """find, import, and return all migration files as modules"""
+        fileloc = self.getconfig('migrate', 'location')
+        fullpath = os.path.join(os.getcwd(), fileloc)
+        try:
+            filenames = os.listdir(fullpath)
+        except FileNotFoundError:
+            self.logger.error('unable to find migration folder "{}"'.format(fullpath))
+            return
+        filenames = filter(lambda x: x.startswith('version_') and x.endswith('.py'), filenames)
+        filepaths = [(os.path.join(fullpath, f), f.replace('.py', '')) for f in filenames]
+        migrations = []
+        for fp, mn in filepaths:
+            module_name = '.'.join([fileloc.replace('/','.').strip('.'), mn])
+            try:
+                ver = int(re.search(r'version_(\d+)', mn).group(1))
+            except:
+                self.logger.warning('cannot parse version number from \'{}\', '
+                                    'skipping'.format(mn))
+                continue
+            self.logger.debug('found {} at version {}'.format(module_name, ver))
+            migrations.append((module_name, ver, importlib.import_module(module_name)))
+        return sorted(migrations, key=lambda x: x[1])
+
+    def getconfig(self, section, key):
+        config = configparser.ConfigParser()
+        config.read('jambi.conf')
+        return config[section][key]
 
     def inspect(self):
         """inspect the database and report its version"""
@@ -42,24 +102,46 @@ class Jambi(object):
         try:
             jambi_versions = JambiModel.select().limit(1)
             if any(jambi_versions):
-                self.logger.info('jambi found ref "{}"'.format(jambi_versions[0].ref))
-                result = jambi_versions[0]
+                field = jambi_versions[0].ref
+                try:
+                    result = int(field)
+                except ValueError:
+                    self.logger.error('unable to parse current version \'{}\''
+                                      'as an integer'.format(jambi_versions[0].ref))
+                self.logger.info('your database is at version "{}"'.format(field))
             else:
                 self.logger.info('this database hasn\'t been migrated yet')
         except ProgrammingError:
-            # table is not created
             self.logger.info('run \'init\' to create a jambi version table first')
-            result = None
         finally:
             self.db.close()
         return result
 
+    def __set_version(self, ref):
+        """sets the jambi table version
+
+        Note that this does not run the migrations, but is instead used by
+        the migration logic to easily set the version after migrations have
+        completed.
+        """
+        JambiModel.delete().execute()
+        JambiModel.create(ref=str(ref))
+        self.logger.info('set jambi version to {}'.format(ref))
+
     def init(self):
         """initialize the jambi database version table"""
         self.db.connect()
-        self.db.create_tables([JambiModel], safe=True)
+        try:
+            self.db.create_tables([JambiModel], safe=True)
+            JambiModel.create(ref='0')
+            self.logger.info('database initialized')
+        except IntegrityError:
+            self.logger.info('database was already initialized')
         self.db.close()
-        self.logger.info('jambi was initialized')
+
+    def makemigration(self, template=None):
+        """create a new migration from template and place in migrate location"""
+        return NotImplemented
 
     def wish_from_kwargs(self, **kwargs):
         """Processes keyword arguments in to a jambi wish."""
@@ -81,13 +163,11 @@ class Jambi(object):
         return result
 
     def __get_db_and_schema_from_config(self):
-        config = configparser.ConfigParser()
-        config.read('jambi.conf')
-        _db.init(config['database']['database'],
-                 user=config['database']['user'],
-                 password=config['database']['password'],
-                 host=config['database']['host'])
-        _schema = config['database']['schema']
+        _db.init(self.getconfig('database', 'database'),
+                 user=self.getconfig('database', 'user'),
+                 password=self.getconfig('database', 'password'),
+                 host=self.getconfig('database', 'host'))
+        _schema = self.getconfig('database', 'schema')
         return _db, _schema
 
 
