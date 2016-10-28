e7c456319ede24f609ec62038e4a034c229dc6f2
Steven Smith <ssmith@perdixsw.com>
2016-10-27 22:01:22 -0400

    Better config handling, including comments from PR #11. Also added --version to command line

---
+++ jambi/config.py
@@ -0,0 +1,22 @@
+import os
+
+from jambi.exceptions import ImproperlyConfigured
+
+ENVIRONMENT_VARIABLE = "JAMBI_CONFIG"
+
+def get_config_file():
+    config_file = os.environ.get(ENVIRONMENT_VARIABLE)
+    if config_file:
+        if os.path.isfile(config_file):
+            return config_file
+        else:
+            raise ImproperlyConfigured(
+                "{} environment variable is set, but no file was found" \
+                .format(ENVIRONMENT_VARIABLE)
+            )
+    else:
+        raise ImproperlyConfigured(
+            "No configuration file found. Either pass an absolute path " \
+            "when you invoke jambi, or set the {} environment variable." \
+            .format(ENVIRONMENT_VARIABLE)
+        )
